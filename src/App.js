8ead1971df501bdafe5cef9cec5c38c7c0a298d2
othaderek <otha.hernandez@gmail.com>
2021-03-07 17:09:41 -0500

    changes made. :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      exchanges: null
+    }
+  }
+  componentDidMount(){
+    this.getExchanges();
+    console.log(this.state.exchanges);
+  }
+  async getExchanges(){
+    const res = await axios.get('http://localhost:3000/exchanges')
+    const data = await res.data
+
+    this.setState({
+      exchanges: data
+    })
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default App;
